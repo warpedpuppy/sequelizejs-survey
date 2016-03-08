164e9ac2fca0ca2b3307007032c2103846c20464
ted <ted@warpedpuppy.com>
2016-03-08 07:45:20 -0800

    asdf

--- routes/index.js
+++ routes/index.js
@@ -59,10 +59,12 @@ router.get('/', function(req, res, next) {
     //THIS IS ONLY HERE SO I CAN MAKE SURE THE FIRST TIME THIS IS USED IT DOESN'T BREAK
         models.answers_ew_test.drop();
         models.question_ew_test.drop();*/
+    answers = [];
 
 
   models.answers_ew_test.findAll().then(function(answers) {
   var showform = 1;
+      console.log("answers = "+answers)
     res.render('index', {
         showform: showform,
         answers: answers
